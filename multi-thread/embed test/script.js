b9ffa9af50d24929a00bb1bb99f054b0571a2af0
Ev <ev>
2023-08-14 04:26:34 +0200

    test

--- multi-thread/embed test/script.js
+++ multi-thread/embed test/script.js
@@ -3,10 +3,47 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultDiv = document.getElementById('result');
     const sendToThreadCheckbox = document.getElementById('sendToThread');
     const threadIdInput = document.getElementById('threadId');
+    const importThreadsInput = document.getElementById('importThreads');
+    const threadListDiv = document.getElementById('threadList');
 
     sendToThreadCheckbox.addEventListener('change', () => {
         threadIdInput.disabled = !sendToThreadCheckbox.checked;
     });
+
+    importThreadsInput.addEventListener('change', (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            parseThreadIdsFromFile(file);
+        }
+    });
+
+    function parseThreadIdsFromFile(file) {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const threadIds = event.target.result.split('\n');
+            displayThreadIds(threadIds);
+        };
+        reader.readAsText(file);
+    }
+
+    function displayThreadIds(threadIds) {
+        threadListDiv.innerHTML = "";
+        threadIds.forEach((threadId) => {
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.name = 'selectedThreads';
+            checkbox.value = threadId.trim();
+            checkbox.id = `thread_${threadId}`;
+
+            const label = document.createElement('label');
+            label.htmlFor = `thread_${threadId}`;
+            label.textContent = threadId;
+
+            threadListDiv.appendChild(checkbox);
+            threadListDiv.appendChild(label);
+            threadListDiv.appendChild(document.createElement('br'));
+        });
+    }
 
     webhookForm.addEventListener('submit', async (e) => {
         e.preventDefault();
@@ -17,10 +54,17 @@ document.addEventListener('DOMContentLoaded', () => {
         const sendToThread = sendToThreadCheckbox.checked;
         const threadId = threadIdInput.value;
 
+        let selectedThreadIds = [];
+        document.querySelectorAll('input[name="selectedThreads"]:checked').forEach((checkbox) => {
+            selectedThreadIds.push(checkbox.value);
+        });
+
         let url = webhookUrl;
 
         if (sendToThread && threadId.trim() !== "") {
             url += `?thread_id=${threadId}`;
+        } else if (sendToThread && selectedThreadIds.length > 0) {
+            url += `?thread_id=${selectedThreadIds.join(',')}`;
         }
 
         const payload = {
